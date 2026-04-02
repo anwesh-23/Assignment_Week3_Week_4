52fa835c5b6a1f9d4c624595a02d424c6bceb117
anwesh-23 <anweshdash295@gmail.com>
2026-04-02 15:58:12 +0530

    Problem 2: Client Risk Score Ranking

--- src/Assignment_3_4.java
+++ src/Assignment_3_4.java
@@ -1,42 +1,35 @@
-import java.util.*;
-
 class Assignment_3_4 {
 
-    static class Transaction {
-        String id;
-        double fee;
-        String ts;
+    static class Client {
+        String name;
+        int risk;
 
-        Transaction(String id, double fee, String ts) {
-            this.id = id;
-            this.fee = fee;
-            this.ts = ts;
+        Client(String n, int r) {
+            name = n;
+            risk = r;
         }
     }
 
-    static void bubbleSort(List<Transaction> list) {
-        int n = list.size();
-        for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (list.get(j).fee > list.get(j + 1).fee) {
-                    Collections.swap(list, j, j + 1);
-                    swapped = true;
+    static void bubbleSort(Client[] arr) {
+        for (int i = 0; i < arr.length - 1; i++)
+            for (int j = 0; j < arr.length - i - 1; j++)
+                if (arr[j].risk > arr[j + 1].risk) {
+                    Client temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
-            }
-            if (!swapped) break;
-        }
     }
 
     public static void main(String[] args) {
-        List<Transaction> list = new ArrayList<>();
-        list.add(new Transaction("id1", 10.5, "10:00"));
-        list.add(new Transaction("id2", 25.0, "09:30"));
-        list.add(new Transaction("id3", 5.0, "10:15"));
+        Client[] arr = {
+                new Client("A", 20),
+                new Client("B", 50),
+                new Client("C", 80)
+        };
 
-        bubbleSort(list);
+        bubbleSort(arr);
 
-        for (Transaction t : list)
-            System.out.println(t.id + " : " + t.fee);
+        for (Client c : arr)
+            System.out.println(c.name + " " + c.risk);
     }
 }
