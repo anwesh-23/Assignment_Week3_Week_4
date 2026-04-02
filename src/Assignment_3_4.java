8319e9a6b24aa7a84e9119257f5e702c48c5661f
anwesh-23 <anweshdash295@gmail.com>
2026-04-02 16:01:48 +0530

    Problem 5: Account ID Lookup

--- src/Assignment_3_4.java
+++ src/Assignment_3_4.java
@@ -1,48 +1,14 @@
-import java.util.*;
-
 class Assignment_3_4 {
 
-    static class Asset {
-        String name;
-        double rate;
-
-        Asset(String n, double r) {
-            name = n;
-            rate = r;
-        }
-    }
-
-    static void quickSort(List<Asset> list, int low, int high) {
-        if (low < high) {
-            int pi = partition(list, low, high);
-            quickSort(list, low, pi - 1);
-            quickSort(list, pi + 1, high);
-        }
-    }
-
-    static int partition(List<Asset> list, int low, int high) {
-        double pivot = list.get(high).rate;
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (list.get(j).rate > pivot) {
-                i++;
-                Collections.swap(list, i, j);
-            }
-        }
-        Collections.swap(list, i + 1, high);
-        return i + 1;
+    static int linearSearch(String[] arr, String key) {
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i].equals(key))
+                return i;
+        return -1;
     }
 
     public static void main(String[] args) {
-        List<Asset> list = new ArrayList<>();
-        list.add(new Asset("AAPL", 12));
-        list.add(new Asset("TSLA", 8));
-        list.add(new Asset("GOOG", 15));
-
-        quickSort(list, 0, list.size() - 1);
-
-        for (Asset a : list)
-            System.out.println(a.name + " " + a.rate);
+        String[] arr = {"accA", "accB", "accB", "accC"};
+        System.out.println(linearSearch(arr, "accB"));
     }
 }
